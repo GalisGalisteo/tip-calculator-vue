7b6285834e93586855ebfad178c9062b277abbe8
GalisGalisteo <galis.travelling@gmail.com>
2023-11-08 12:47:23 +0100

    finishing ex

---
+++ app.js
@@ -0,0 +1,28 @@
+import {
+  createApp,
+  ref,
+  computed,
+} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp({
+  setup() {
+    const billAmount = ref("");
+    const percentageTip = ref("");
+    const tipAmount = ref("");
+    const total = ref("");
+    
+    const calculate = () => {
+        console.log(typeof billAmount.value)
+      tipAmount.value = (billAmount.value * percentageTip.value) / 100;
+      total.value = billAmount.value + tipAmount.value;
+    };
+
+    return {
+      billAmount,
+      percentageTip,
+      tipAmount,
+      total,
+      calculate,
+    };
+  },
+}).mount("#app");
